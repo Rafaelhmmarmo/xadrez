9906324dd90aa376df0059252e82b83f85d1250c
Rafael Henrique <rafaelhmmarmo@gmail.com>
2019-11-02 16:41:48 -0300

    Melhorando implementacao

--- Xadrez-console/Program.cs
+++ Xadrez-console/Program.cs
@@ -8,9 +8,22 @@ namespace Xadrez_console
     {
         static void Main(string[] args)
         {
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Console.WriteLine(pos.toPosicao());
+                tab.colocaPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocaPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocaPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+
+                tab.colocaPeca(new Rei(tab, Cor.Branca), new Posicao(3, 5));
+
+                Tela.ImprimirTabuleiro(tab);
+            }
+            catch(TabuleiroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
             
                   
